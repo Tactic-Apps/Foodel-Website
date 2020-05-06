8b2849b708a7004b6e8a4066298c6fb7be959d72
RayB <raybernard1337@gmail.com>
2020-05-06 06:13:28 -0500

    Updated component images

--- src/components/Cities/Cities.js
+++ src/components/Cities/Cities.js
@@ -1,93 +1,91 @@
-import React from 'react';
-import ScrollableAnchor from 'react-scrollable-anchor';
-import {Animated} from "react-animated-css";
+import React from "react";
+import ScrollableAnchor from "react-scrollable-anchor";
+import { Animated } from "react-animated-css";
 
-import City from './City/City';
-import lisbon from '../../assets/img/lisbon-3-min.jpg';
-import sanfrancisco from '../../assets/img/san-francisco-min.jpg';
-import berlin from '../../assets/img/berlin-min.jpg';
-import london from '../../assets/img/london-min.jpg';
-import './Cities.css';
+import City from "./City/City";
+import jacksonville from "../../assets/img/jacksonville-min.jpg";
+import gainesville from "../../assets/img/gainesville-min.jpg";
+import orlando from "../../assets/img/orlando-min.jpg";
+import tampa from "../../assets/img/tampa-min.jpg";
+import "./Cities.css";
 
 const cities = {
-    Lisbon: {
-        cityName: 'Lisbon',
-        img: lisbon,
-        customerIcon: 'person',
-        chefIcon: 'star',
-        socialMediaIcon: 'logo-twitter',
-        customers: '1600+ happy eaters',
-        chefs: '60+ top chefs',
-        twitterHandle: '@omnifood_lx'
-    },
-    SanFrancisco: {
-        cityName: 'San Francisco',
-        img: sanfrancisco,
-        customerIcon: 'person',
-        chefIcon: 'star',
-        socialMediaIcon: 'logo-twitter',
-        customers: '3700+ happy eaters',
-        chefs: '160+ top chefs',
-        twitterHandle: '@omnifood_sf'
-    },
-    Berlin: {
-        cityName: 'Berlin',
-        img: berlin,
-        customerIcon: 'person',
-        chefIcon: 'star',
-        socialMediaIcon: 'logo-twitter',
-        customers: '2300+ happy eaters',
-        chefs: '110+ top chefs',
-        twitterHandle: '@omnifood_berlin'
-    },
-    London: {
-        cityName: 'London',
-        img: london,
-        customerIcon: 'person',
-        chefIcon: 'star',
-        socialMediaIcon: 'logo-twitter',
-        customers: '1200+ happy eaters',
-        chefs: '50+ top chefs',
-        twitterHandle: '@omnifood_london'
-    }
-}
+  Jacksonville: {
+    cityName: "Jacksonville",
+    img: jacksonville,
+    customerIcon: "person",
+    chefIcon: "star",
+    socialMediaIcon: "logo-twitter",
+    customers: "1600+ happy eaters",
+    chefs: "60+ top chefs",
+    twitterHandle: "@foodel_jv",
+  },
+  Gainesville: {
+    cityName: "Gainesville",
+    img: gainesville,
+    customerIcon: "person",
+    chefIcon: "star",
+    socialMediaIcon: "logo-twitter",
+    customers: "3700+ happy eaters",
+    chefs: "160+ top chefs",
+    twitterHandle: "@foodel_gv",
+  },
+  Orlando: {
+    cityName: "Orlando",
+    img: orlando,
+    customerIcon: "person",
+    chefIcon: "star",
+    socialMediaIcon: "logo-twitter",
+    customers: "2300+ happy eaters",
+    chefs: "110+ top chefs",
+    twitterHandle: "@foodel_orlando",
+  },
+  Tampa: {
+    cityName: "Tampa",
+    img: tampa,
+    customerIcon: "person",
+    chefIcon: "star",
+    socialMediaIcon: "logo-twitter",
+    customers: "1200+ happy eaters",
+    chefs: "50+ top chefs",
+    twitterHandle: "@foodel_tampa",
+  },
+};
 
 const cityDisplay = () => {
-    let cityArray = [];
-    for(let city in cities) {
-        cityArray.push((
-            <City 
-                key={cities[city].cityName} 
-                img={cities[city].img} 
-                alt={cities[city].cityName} 
-                city={cities[city].cityName} 
-                customerIcon={cities[city].customerIcon} 
-                customers={cities[city].customers} 
-                chefIcon={cities[city].chefIcon} 
-                chefs={cities[city].chefs} 
-                socialMediaIcon={cities[city].socialMediaIcon} 
-                twitterHandle={cities[city].twitterHandle}
-            />
-        ));
-    }
-    return cityArray;
-}
-
-const Cities = props => {
-    return (
-        <ScrollableAnchor id={'cities'}>
-            <Animated animationIn="fadeIn" isVisible={props.cityFade}>    
-            <section className="section-cities">
-                    <div className="row">
-                        <h2>We're currently in these cities</h2>
-                    </div>
-                    <div className="row">
-                        {cityDisplay()}
-                    </div>
-                </section>
-            </Animated>
-        </ScrollableAnchor>
+  let cityArray = [];
+  for (let city in cities) {
+    cityArray.push(
+      <City
+        key={cities[city].cityName}
+        img={cities[city].img}
+        alt={cities[city].cityName}
+        city={cities[city].cityName}
+        customerIcon={cities[city].customerIcon}
+        customers={cities[city].customers}
+        chefIcon={cities[city].chefIcon}
+        chefs={cities[city].chefs}
+        socialMediaIcon={cities[city].socialMediaIcon}
+        twitterHandle={cities[city].twitterHandle}
+      />
     );
-}
+  }
+  return cityArray;
+};
+
+const Cities = (props) => {
+  return (
+    <ScrollableAnchor id={"cities"}>
+      <Animated animationIn="fadeIn" isVisible={props.cityFade}>
+        <section className="section-cities">
+          <div className="row">
+            <h2>We're currently in these cities</h2>
+          </div>
+          <div className="row">{cityDisplay()}</div>
+        </section>
+      </Animated>
+    </ScrollableAnchor>
+  );
+};
 
 export default Cities;
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './Meals.css';
-import Meal from './Meal/Meal';
+import "./Meals.css";
+import Meal from "./Meal/Meal";
 import img1 from "../../assets/img/1-min.jpg";
 import img2 from "../../assets/img/2-min.jpg";
 import img3 from "../../assets/img/3-min.jpg";
@@ -12,56 +12,60 @@ import img7 from "../../assets/img/7-min.jpg";
 import img8 from "../../assets/img/8-min.jpg";
 
 const meals = {
-    meal1: {
-        imageName: img1,
-        imageAlt: "Korean bibimbap with egg and vegetables"
-    },
-    meal2: {
-        imageName: img2,
-        imageAlt: "Simple italian pizza with cherry tomatoes"
-    },
-    meal3: {
-        imageName: img3,
-        imageAlt: "Chicken breast steak with vegetables"
-    },
-    meal4: {
-        imageName: img4,
-        imageAlt: "Autumn pumpkin soup"
-    },
-    meal5: {
-        imageName: img5,
-        imageAlt: "Paleo beef steak with vegetables"
-    },
-    meal6: {
-        imageName: img6,
-        imageAlt: "Healthy baguette with egg and vegetables"
-    },
-    meal7: {
-        imageName: img7,
-        imageAlt: "Burger with cheddar and bacon"
-    },
-    meal8: {
-        imageName: img8,
-        imageAlt: "Granola with cherries and strawberries"
-    }
-}
+  meal1: {
+    imageName: img1,
+    imageAlt: "Korean bibimbap with egg and vegetables",
+  },
+  meal2: {
+    imageName: img2,
+    imageAlt: "Salmon with vegetables",
+  },
+  meal3: {
+    imageName: img3,
+    imageAlt: "Chicken breast steak with vegetables",
+  },
+  meal4: {
+    imageName: img4,
+    imageAlt: "Autumn pumpkin soup",
+  },
+  meal5: {
+    imageName: img5,
+    imageAlt: "Granola with fruit and nuts",
+  },
+  meal6: {
+    imageName: img6,
+    imageAlt: "Healthy baguette with egg and vegetables",
+  },
+  meal7: {
+    imageName: img7,
+    imageAlt: "Complex salad with chicken",
+  },
+  meal8: {
+    imageName: img8,
+    imageAlt: "Paleo beef steak with vegetables",
+  },
+};
 
 const mealList = () => {
-    let mealArray = [];
-    for(let meal in meals) {
-        mealArray.push((<Meal key={meals[meal].imageName} img={meals[meal].imageName} alt={meals[meal].imageAlt} />));
-    }
-    return mealArray;
-}
-
-const Meals = props => {
-    return (
-        <section className="section-meals">
-            <ul className="meals-showcase clearfix">
-                {mealList()}
-            </ul>
-        </section>
+  let mealArray = [];
+  for (let meal in meals) {
+    mealArray.push(
+      <Meal
+        key={meals[meal].imageName}
+        img={meals[meal].imageName}
+        alt={meals[meal].imageAlt}
+      />
     );
-}
+  }
+  return mealArray;
+};
+
+const Meals = (props) => {
+  return (
+    <section className="section-meals">
+      <ul className="meals-showcase clearfix">{mealList()}</ul>
+    </section>
+  );
+};
 
 export default Meals;
